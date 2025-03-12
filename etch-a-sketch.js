cd7cdf4b6bf04be0b672c7981772d886e75136e5
NarsU-Githbb <anssi.peltoniemi@hotmail.com>
2025-03-12 16:38:25 +0200

    Add Background-Color To Divs Onclick
    Updated my makeDivs fuction & now it should add backgroud-color to my divs on click.
    Next stop: trying to make it more specific!

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,23 @@
 
 const container = document.getElementById('container');
+//const color = document.getElementsByTagName('div');
 
 function makeDivs(numDivs) {
 for (let i = 0; i < numDivs; i++){
     let gridCells = document.createElement('div');
+    gridCells.classList.add('grid-item');
+        
+       
+        gridCells.addEventListener('click', function() {
+            gridCells.style.backgroundColor = 'blue';  
+        });
     container.appendChild(gridCells);    
     }
     }
+    makeDivs(256);
 
-makeDivs(256);
+    //container.addEventListener(onclick, (div) => {
+    //container.classList.add('hidden');
+
+
+    //});
