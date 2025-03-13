ed382b8083effbfd1108b83d46024ce5d24e042c
NarsU-Githbb <anssi.peltoniemi@hotmail.com>
2025-03-13 16:33:41 +0200

    Add Background-Color Randomizer
    Added a randomizer that will change the drawing color everytime you refresh the browser.
    Will try to link the randomizer to a eraser button somehow when I decide to make one!

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,17 +1,19 @@
 
 const container = document.getElementById('container');
-
+let red = Math.floor(Math.random() * 224) + 1;
+    let green = Math.floor(Math.random() * 224) + 1;
+    let blue = Math.floor(Math.random() * 224) + 1;
 
 function makeDivs(numDivs) {
 for (let i = 0; i < numDivs; i++){
     let gridCells = document.createElement('div');
     gridCells.classList.add('grid-item');
-        
+    
        let opacity = 0;
         gridCells.addEventListener('mouseover', function() {
             opacity += 0.1;
             if (opacity > 1) opacity = 1;
-            gridCells.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;  
+            gridCells.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${opacity})`;  
         });
     container.appendChild(gridCells);    
     }
