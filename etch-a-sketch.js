0bf277a905b6a6accd1ba941ad4c759c61ce793e
NarsU-Githbb <anssi.peltoniemi@hotmail.com>
2025-03-22 18:17:52 +0200

    Add restart button
    Button will delete all the gridcells in the original container & replace them with empty new ones.
    It also asks how big the new grid should be (max = 100x100) thus you can make your drawings more detailed.
    I also deleted grid-item borders coz I thought they intefere with drawings.
    
    Still need to do some finetuning on the CSS.
    Also I'm trying to make the background color change everytime you trigger restart function
    (but don't seem to understand how I should go about it). So maybe I skip that one or not (will see).

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,23 +1,53 @@
 
 const container = document.getElementById('container');
+const buttonOne = document.getElementById('eraser');
 let red = Math.floor(Math.random() * 224) + 1;
     let green = Math.floor(Math.random() * 224) + 1;
     let blue = Math.floor(Math.random() * 224) + 1;
+let numDivs = 256;
 
-function makeDivs(numDivs) {
 for (let i = 0; i < numDivs; i++){
     let gridCells = document.createElement('div');
     gridCells.classList.add('grid-item');
-    
+   
        let opacity = 0;
         gridCells.addEventListener('mouseover', function() {
-            opacity += 0.1;
+            opacity += 0.2;
             if (opacity > 1) opacity = 1;
-            gridCells.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${opacity})`;  
+            gridCells.style.backgroundColor = `rgba(${red},${green},${blue}, ${opacity})`  
         });
     container.appendChild(gridCells);    
     }
-    }
-    makeDivs(256);
-
    
+
+
+    
+
+    function restart(moreDivs) {
+       
+        container.innerHTML = '';
+    
+
+        for (let i = 0; i < moreDivs; i++) {
+            let gridCells = document.createElement('div');
+            gridCells.classList.add('grid-item');
+            
+            let opacity = 0;
+            gridCells.addEventListener('mouseover', function() {
+                opacity += 0.2;
+                if (opacity > 1) opacity = 1;
+                
+                gridCells.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${opacity})`;
+            });
+            container.appendChild(gridCells);
+        }
+    }
+    
+ 
+      
+    buttonOne.addEventListener('click', function() {
+        let newGridSize = prompt("how big u want it?");
+        if (newGridSize > 100) newGridSize = 100;
+        let GridRestart = newGridSize * newGridSize;
+        restart(GridRestart)
+    })
